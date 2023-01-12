8b736d689c0e44399a9c8bc999fe45c405927469
MK <MK@HP-V>
2023-01-12 09:52:50 +0100

    Add project files.

---
+++ DapperSqlConnection/Program.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Configuration;
+
+namespace DapperSqlConnection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Połączono");
+                var result = connection.Query<User>("select * FROM [Dapper].[dbo].[User]");
+
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item.Id);
+                    Console.WriteLine(item.Name);
+                    Console.WriteLine(item.Surname);
+                }
+            }
+        }
+    }
+}
+
